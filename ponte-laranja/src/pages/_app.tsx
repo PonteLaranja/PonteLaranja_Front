65a149aea8dcda8e68e3f736f4a561f04cb7e58f
nicollasOR <nicollas.oliveira.rufino30@gmail.com>
2026-06-10 14:55:59 -0300

    :wrench: chore: adjust dependencies :wrench:

--- ponte-laranja/src/pages/_app.tsx
+++ ponte-laranja/src/pages/_app.tsx
@@ -1,17 +1,14 @@
-import "@/styles/globals.css";
+import "../styles/globals.css"
 import type { AppProps } from "next/app";
 
-import Head from "next/head";
-import Image from "next/image";
 import { Montserrat, Plus_Jakarta_Sans } from "next/font/google";
-import styles from "@/styles/Home.module.css";
 
 const montserrat = Montserrat({
   variable: "--font-texto-titulo",
   subsets: ["latin"],
   weight: ["200", "300", "400", "500", "600", "700" , "800", "900"]
 });
-
+// oxi mn
 const jakarta = Plus_Jakarta_Sans({
   variable: "--font-texto-padrao",
   subsets: ["latin"],
@@ -19,7 +16,9 @@ const jakarta = Plus_Jakarta_Sans({
 });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return(
-    <main className={`${jakarta.variable} ${montserrat.variable}`}></main>
-  )
+  return (
+    <main className={`${jakarta.variable} ${montserrat.variable}`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
