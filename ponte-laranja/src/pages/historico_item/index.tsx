9bbdf56571f92acc7098fb636545be3830d2e652
nicollasOR <nicollas.oliveira.rufino30@gmail.com>
2026-06-12 10:07:17 -0300

    :art: feat: start sub_header :art:

--- ponte-laranja/src/pages/historico_item/index.tsx
+++ ponte-laranja/src/pages/historico_item/index.tsx
@@ -1,11 +1,13 @@
 import { Header } from "@/src/components/header/Header"
+import { SubHeader } from "@/src/components/sub-header/subHeader"
+
 
 
 const Historico = () => {
 
     return(
         <>
-        <Header/>        
+        
         </>
     )
 }
