9600bba2afa0eb99b2debc9ccafabd0c96b5fd67
nicollasOR <nicollas.oliveira.rufino30@gmail.com>
2026-06-12 03:15:04 -0300

    :sparkles: feat: login html && css finish :sparkles:

--- ponte-laranja/src/pages/login/index.tsx
+++ ponte-laranja/src/pages/login/index.tsx
@@ -1,24 +1,30 @@
-import styles from './login.module.css'
-
+import styles from "./login.module.css";
 
 const Login = () => {
+  return (
+    <main className={styles.main}>
+      <div className={styles.img}>
+        <img src="/img/logo-banner.png" alt="" />
+      </div>
+      <form className={styles.login}>
+        <h1>LOGIN</h1>
+        <div className={styles.inserir}>
+          <div className={styles.inserir_dados}>
+            <label htmlFor="">Email</label>
+            <input type="text" />
+          </div>
+          <div className={styles.inserir_dados}>
+            <label htmlFor="" id={styles.senha}>Senha</label>
+            <div id={styles.inserir_senha}>
+              <input type="text" />
+              <img src="/svg/olho.svg" alt="" />
+            </div>
+          </div>
+        </div>
+        <button>Entrar</button>
+      </form>
+    </main>
+  );
+};
 
-
-
-
-
-    return (
-        <main className={styles.main}>
-            <div><img src="/img/logo-banner.png" alt="" /></div>
-            <form className={styles.login}>
-                <h1>LOGIN</h1>
-                <div className={styles.inserir}>
-                    <div className={styles.inserir_dados}><label htmlFor="">Email</label><input type="text" /></div>
-                    <div className={styles.inserir_dados}><label htmlFor="">Senha</label><input type="text" /></div></div>
-                <button>Entrar</button>
-            </form>
-        </main>
-    )
-}
-
-export default Login
+export default Login;
