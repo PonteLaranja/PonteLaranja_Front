9b40b2c59d2bc9eb27852a8e031f0a790b852930
DIEGO CASTRO DA ROCHA <45255137804@info.local>
2026-06-18 11:06:42 -0300

    feat:TerminarLogin

--- ponte-laranja/src/pages/login/index.tsx
+++ ponte-laranja/src/pages/login/index.tsx
@@ -8,19 +8,16 @@ const Login = () => {
       </div>
       <form className={styles.login}>
         <h1>LOGIN</h1>
-        <div className={styles.inserir}>
-          <div className={styles.inserir_dados}>
+        <div>
+          <div>
             <label htmlFor="">Email</label>
-            <input type="text" />
+            <input type="email" placeholder="email@gmail.com"></input>
           </div>
-          <div className={styles.inserir_dados}>
-            <label htmlFor="" id={styles.senha}>Senha</label>
-            <div id={styles.inserir_senha}>
-              <input type="text" />
-              <img src="/svg/olho.svg" alt="" />
-            </div>
+          <div>
+            <label htmlFor="">Senha</label>
+            <input type="password"></input>
           </div>
-        </div>
+        </div>  
         <button>Entrar</button>
       </form>
     </main>
