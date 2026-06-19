3ec3b94762fe794f52427985b6dc10a1ce17aa0a
DIEGO CASTRO DA ROCHA <45255137804@info.local>
2026-06-19 10:32:00 -0300

    feat:loginPronto

--- ponte-laranja/src/pages/login/index.tsx
+++ ponte-laranja/src/pages/login/index.tsx
@@ -8,17 +8,24 @@ const Login = () => {
       </div>
       <form className={styles.login}>
         <h1>LOGIN</h1>
-        <div>
-          <div>
+        <div className={styles.LabelInput}>
+          <div className={styles.email}>
             <label htmlFor="">Email</label>
+            <div>
             <input type="email" placeholder="email@gmail.com"></input>
+            </div>
           </div>
-          <div>
+          <div className={styles.senha}>
             <label htmlFor="">Senha</label>
+            <div>
             <input type="password"></input>
+            </div>
           </div>
-        </div>  
+        </div> 
+        <div className={styles.botoes}>
         <button>Entrar</button>
+        </div> 
+        <p>Não possui cadastro? <a href="">Click aqui</a></p>
       </form>
     </main>
   );
