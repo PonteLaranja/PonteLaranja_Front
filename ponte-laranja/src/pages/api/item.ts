79d88e266d5716d23148b036eb7c2fa5b9bfc8d9
DIEGO CASTRO DA ROCHA <45255137804@info.local>
2026-06-22 12:54:14 -0300

    feat:cadastoItemFaltaPost

--- ponte-laranja/src/pages/api/item.ts
+++ ponte-laranja/src/pages/api/item.ts
@@ -1,7 +1,6 @@
 import { api } from "./api";
 
 export interface Item {
-  itemID: string;
   nomeItem: string;
   dataRecebimento: Date;
   dataDoacao: Date;
@@ -13,12 +12,35 @@ export interface Item {
   unidadeID: string;
 }
 
+export async function adicionarItem(item: Item) {
+    try{
+
+        const formData = new FormData
+
+        formData.append("Nome", item.nomeItem)
+        formData.append("Recebimento", item.dataRecebimento.toDateString())
+        formData.append("Doacao",item.dataDoacao.toDateString())
+        formData.append("Medida", item.medida.toString())
+        formData.append("Quantidade", item.quantidade.toString())
+        formData. append("TipoItem", item.tipoItemID)
+        formData. append("TipoMedida", item.tipoMedidaID)
+        formData. append("Usuario", item.usuarioID)
+        formData. append("Unidade", item.unidadeID)
+        
+        await api.post("Item", formData)
+    }
+    catch(error: any){
+        throw new Error(error.response.data)
+
+    }
+}
+
 export async function listarItens() {
     try
     {
         const response = await api.get("Item")
         console.log(response.data)
-        return response
+        return response.data
     }
 
     catch(erro: any)
@@ -27,7 +49,43 @@ export async function listarItens() {
     }
 }
 
-export async function listarItens_Id()
-{
-    
+export async function listarTipoMedida() {
+    try{
+        const response = await api.get("TipoMedida")
+        return response.data
+    }
+    catch(erro: any){
+        throw new Error(erro.response.data)
+    }
 }
+
+export async function listarTipoItem() {
+    try{
+        const response = await api.get("TipoItem")
+        return response.data
+    }
+    catch(erro: any){
+        throw new Error(erro.response.data)
+    }
+}
+
+export async function getUnidade() {
+    try{
+        const response = await api.get("Unidade")
+        return response.data
+    }
+    catch(erro: any){
+        throw new Error(erro.response.data)
+    }
+}
+
+export async function getUsuario() {
+    try{
+        const response = await api.get("Usuario")
+        return response.data
+    }
+    catch(erro: any){
+        throw new Error(erro.response.data)
+    }
+}
+
--- ponte-laranja/src/pages/cadastroitem/index.tsx
+++ ponte-laranja/src/pages/cadastroitem/index.tsx
@@ -1,68 +1,169 @@
 import styles from "./cadastroitem.module.css";
-import { Header } from "@/src/components/header/header";
+import { getUnidade, getUsuario, Item, listarItens, listarTipoItem, listarTipoMedida } from "../api/item";
+import { useEffect, useState } from "react";
+
+export interface TipoItem {
+  tipoItemID: string
+  nomeTipoItem: string
+}
+
+export interface TipoMedida {
+  tipoMedidaID: string
+  nomeTipoMedida: string
+}
+
+export interface listarunidade {
+  unidadeDto: string
+  nomeUnidadeDto: string
+  ativo: boolean
+  cepDto: string
+  usuarioResponsavelDto: string
+  nomeResponsavelDto: string
+  tipoUnidadeDto: string
+  nomeTipoUnidadeDto: string
+}
+
+export interface listarusuario {
+  usuarioID: string
+  nome: string
+  email: string
+  dataCriação: string
+  tipoUsuarioID: string
+}
 
 const CadastroItem = () => {
+
+  const [nome, setNome] = useState<string>("")
+  const [quantidade, setQuantidade] = useState<number>()
+  const [medida, setMedida] = useState<number>()
+  const [coleta, setColeta] = useState<Date>()
+  const [doacao, setDoacao] = useState<Date>()
+  const [tipoItem, setTipoItem] = useState<TipoItem[]>([])
+  const [tipoMedida, setTipoMedida] = useState<TipoMedida[]>([])
+  const [listarUnidade, setListarUnidade] = useState<listarunidade[]>([])
+  const [listarUsuario, setListarUsuario] = useState<listarusuario[]>([])
+
+  async function salvaritem(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        try {
+
+          const dados = {
+              
+          }
+
+        }
+        catch{
+
+        }
+      }
+  async function ListarItens() {
+    const itens: TipoItem[] = await listarTipoItem();
+    setTipoItem(itens)
+  }
+
+  async function ListarMedida() {
+    const medidas: TipoMedida[] = await listarTipoMedida();
+    setTipoMedida(medidas)
+  }
+
+  async function ListarUnidade() {
+    const unidades: listarunidade[] = await getUnidade();
+    setListarUnidade(unidades.filter(U => U.ativo == true))
+  }
+
+  async function ListarUsuario() {
+    const usuarios: listarusuario[] = await getUsuario();
+    setListarUsuario(usuarios)
+  }
+
+  useEffect(() => {
+    ListarItens();
+    ListarMedida();
+    ListarUnidade();
+    ListarUsuario();
+  }, []);
+
   return (
     <>
-    <Header></Header>
-    <main className={styles.main}>  
-      <h2>Cadastro de Itens</h2>
-      <form className={styles.form}>
-      <div className={styles.primeiraLinha}>
-        <div className={styles.nome}>
-          <h4>Nome</h4>
-          <input type="text" />
-        </div>
-        <div className={styles.unidade}>
-          <h4>UN</h4>
-          <input type="text" />
-        </div>
-      </div>
-      <div className={styles.segundaLinha}>
-        <div className={styles.medida}>
-          <h4>Medida</h4>
-          <input type="text" />
-        </div>
-        <div className={styles.coleta}>
-          <h4>Coleta</h4>
-          <input type="date" />
-        </div>
-        <div className={styles.doacao}>
-          <h4>Doação</h4>
-          <input type="date" />
-        </div>
-      </div>
-      <div className={styles.terceiraLinha}>
-        <div className={styles.TipoMedida}>
-          <h4>Tipo da Medida</h4>
-          <select>
-            <option value="kg">kg</option>
-          </select>
-        </div>
-        <div className={styles.TipoItem}>
-          <h4>Tipo de Item</h4>
-          <select>
-            <option value="brinquedo">Brinquedo</option>
-          </select>
-        </div>
-        <div className={styles.Unidade}>
-          <h4>Unidade</h4>
-          <select>
-            <option value="santo andre">Santo André</option>
-          </select>
-        </div>
-      </div>
-      <div className={styles.quartaLinha}>
-        <div className={styles.Responsavel}>
-          <h4>Responsável</h4>
-          <select>
-            <option value="nicollas">Nicollas</option>
-          </select>
-        </div>
-      </div>
-      <button>Cadastrar</button>
-      </form>
-    </main>
+      <main className={styles.main}>
+        <h2>Cadastro de Itens</h2>
+        <form className={styles.form}>
+          <div className={styles.primeiraLinha}>
+            <div className={styles.nome}>
+              <h4>Nome</h4>
+              <input type="text" value={nome} onChange={(e) => setNome(e.target.value)} />
+            </div>
+            <div className={styles.unidade}>
+              <h4>UN</h4>
+              <input type="text" value={quantidade} onChange={(e) => setQuantidade(Number(e.target.value))} />
+            </div>
+          </div>
+          <div className={styles.segundaLinha}>
+            <div className={styles.medida}>
+              <h4>Medida</h4>
+              <input type="text" value={medida} onChange={(e) => setMedida(Number(e.target.value))} />
+            </div>
+            <div className={styles.coleta}>
+              <h4>Coleta</h4>
+              <input type="date" value={coleta?.toLocaleDateString('pt-BR')} onChange={(e) => setColeta(new Date(e.target.value))} />
+            </div>
+            <div className={styles.doacao}>
+              <h4>Doação</h4>
+              <input type="date" value={doacao?.toLocaleDateString('pt-BR')} onChange={(e) => setDoacao(new Date(e.target.value))} />
+            </div>
+          </div>
+          <div className={styles.terceiraLinha}>
+            <div className={styles.TipoMedida}>
+              <h4>Tipo da Medida</h4>
+              <select>
+                {tipoItem.map((item) => (
+                  <option value={item.tipoItemID}>
+                    {item.nomeTipoItem}
+                  </option>
+                )
+                )
+                }
+              </select>
+            </div>
+            <div className={styles.TipoItem}>
+              <h4>Tipo de Item</h4>
+              <select>
+                {tipoMedida.map((medida) =>
+                  <option value={medida.tipoMedidaID}>
+                    {medida.nomeTipoMedida}
+                  </option>
+                )
+                }
+              </select>
+            </div>
+            <div className={styles.Unidade}>
+              <h4>Unidade</h4>
+              <select>
+                {listarUnidade.map((unidade) =>
+                  <option value={unidade.unidadeDto}>
+                    {unidade.nomeUnidadeDto}
+                  </option>
+                )
+                }
+              </select>
+            </div>
+          </div>
+          <div className={styles.quartaLinha}>
+            <div className={styles.Responsavel}>
+              <h4>Responsável</h4>
+              <select>
+                {listarUsuario.map((usuario) =>
+                  <option value={usuario.usuarioID}>
+                    {usuario.nome}
+                  </option>
+                )
+                }
+              </select>
+            </div>
+          </div>
+          <button>Cadastrar</button>
+        </form>
+      </main>
     </>
   );
 };
